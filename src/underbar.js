067acaf0ee1338ba92e951213646504e1cd8df16
rjgcabrera <rjgcengr@gmail.com>
2018-03-06 10:33:53 -0800

    Implement filter and reject
    We also fixed the filter spec.

--- src/underbar.js
+++ src/underbar.js
@@ -80,12 +80,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(elem) {
+      if (test(elem)) {
+        result.push(elem);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function (elem) {
+      return !test(elem);
+    });
   };
 
   // Produce a duplicate-free version of the array.
